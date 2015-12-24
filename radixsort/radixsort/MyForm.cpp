e55d94ccbe968bd3e7ce512894a4d20aa1642a67
Koryun <koryun@live.com>
2015-12-24 21:01:35 +0300

    empty form

---
+++ radixsort/radixsort/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+
+using namespace radixsort;
+
+
+[STAThreadAttribute]
+void main()
+{
+	/*Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);*/
+
+	Application::Run(gcnew MyForm());
+};
+
